eb6a056b6e5aec5255c3fe899a0f18ce23008098
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-09-06 19:13:45 +0200

    get game data

--- src/api/game/services/game.ts
+++ src/api/game/services/game.ts
@@ -4,13 +4,58 @@
 
 import { factories } from '@strapi/strapi';
 import axios from 'axios';
+import { JSDOM } from 'jsdom';
+import slugify from 'slugify';
+
+async function getGameInfo(slug) {
+  const gogSlug = slug.replaceAll('-', '_').toLowerCase();
+
+  const body = await axios.get(`${process.env.GAME_URL}${gogSlug}`);
+  const dom = new JSDOM(body.data);
+
+  const raw_description = dom.window.document.querySelector('.description');
+  const description = raw_description.innerHTML;
+  const short_description = raw_description.textContent.slice(0, 160);
+
+  const ratingElement =
+    dom.window.document
+      .querySelector('.age-restrictions__icon use')
+      ?.getAttribute('xlink:href')
+      .replace(/_/g, ' ')
+      .replace('#', '')
+      .toUpperCase() ?? 'PEGI_3';
+
+  return {
+    description,
+    short_description,
+    ratingElement,
+  };
+}
 
 export default factories.createCoreService('api::game.game', () => ({
   async populate(params) {
     const {
       data: { products },
-    } = await axios.get(process.env.STORE_URL);
+    } = await axios.get(process.env.CATALOG_URL);
 
-    console.log(products);
+    // await getGameInfo('cyberpunk_2077_phantom_liberty');
+
+    products[2].developers.map(async (developer) => {
+      await strapi.service('api::developer.developer').create({
+        data: {
+          name: developer,
+          slug: slugify(developer, { strict: true, lower: true }),
+        },
+      });
+    });
+
+    products[2].publishers.map(async (publisher) => {
+      await strapi.service('api::publisher.publisher').create({
+        data: {
+          name: publisher,
+          slug: slugify(publisher, { strict: true, lower: true }),
+        },
+      });
+    });
   },
 }));
